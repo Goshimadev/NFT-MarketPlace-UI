d35fd1a174b132c19f311444b333fee3fbae6c1d
Ali <qalbeali8@gmail.com>
2022-05-31 16:14:38 +0500

    first commit

--- public/images/Images.tsx
+++ public/images/Images.tsx
@@ -1,11 +1,19 @@
-import next from "./next.png";
-import TP from "./TP.png";
-import tailwind from "./tailwind.png";
-import mui from "./mui.png";
+import logo from './logo.svg'
+import line from './line.svg'
+import profile from './profile.png'
+import heroimg from './heroimg.png'
+import h1 from './h1.png'
+import h2 from './h2.png'
+import h3 from './h3.png'
+import h4 from './h4.png'
 
 export default {
-    next,
-    TP,
-    tailwind,
-    mui
+  logo,
+  profile,
+  heroimg,
+  line,
+  h1,
+  h2,
+  h3,
+  h4,
 }
